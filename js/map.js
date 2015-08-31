d2a075665c39f1fa578802751578db3573e943f3
Robert Garza <[robstudent1988@gmail.com]>
2015-08-31 08:08:16 -0700

    added foodLocations.json, added map location info to map.js

--- js/map.js
+++ js/map.js
@@ -1,10 +1,44 @@
 var map;
 function initMap(){
+	var locations;
+	//location information
+	function locationInformation(){
+		var locations = [];
+
+		for (var spots in foodPlaces.restaurant){
+			locations.push(foodPlaces.restaurant[spots].location);
+		}
+		console.log(locations);
+		return locations;
+	}
+
+	//Map initial location
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 36.8253, lng: -119.7031},
 		zoom: 12
 	});
+
+	//Markers
+	var marker = new google.maps.Marker({
+		animation: google.maps.Animation.DROP,
+		position: {lat: 36.8362263, lng: -119.699005},
+		title: 'Ooi Sushi'
+	});
+	marker.addListener('click', toggleBounce);
+	marker.setMap(map);
+
+	//MAP toggle
+	function toggleBounce(){
+		if (marker.getAnimation() !== null) {
+			marker.setAnimation(null);
+		} else {
+			marker.setAnimation(google.maps.Animation.BOUNCE);
+		}
+	}
+
+	locations = locationInformation();
 };
+
 
 //solution https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
 var input = document.getElementById('pac-input');
