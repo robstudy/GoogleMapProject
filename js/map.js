6c72d8ca449a32e5528de46706eb5180ed1a9860
Robert Garza <[robstudent1988@gmail.com]>
2015-09-01 08:52:32 -0700

    added oaauth.js to index.html and createMarker function added

--- js/map.js
+++ js/map.js
@@ -3,6 +3,8 @@ function initMap(){
 
 	var locations;
 
+	var clovis = {lat: 36.8253, lng: -119.7031};
+
 	//location information
 	function locationInformation(){
 		var locations = [];
@@ -12,25 +14,39 @@ function initMap(){
 		}
 		console.log(locations);
 		return locations;
-	}
+	};
 
 	//Map initial location
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 36.8253, lng: -119.7031},
-		zoom: 12
+		zoom: 14,
+		center: clovis,
+		disableDefaultUI: true
 	});
 
 	//Markers
 	function addMarkers(){
 		for (var spots in foodPlaces.restaurant){
-			var marker = new google.maps.Marker({
-				animation: google.maps.Animation.DROP,
-				position: {lat: foodPlaces.restaurant[spots].lat, lng: foodPlaces.restaurant[spots].lng},
-				title: foodPlaces.restaurant[spots].title
-			});
-			marker.setMap(map);
+			createMarker(foodPlaces.restaurant[spots]);
 		}
-	}
+	};
+
+	//NewCreate Markers
+	function createMarker(place){
+
+		var infowindow;
+
+		var marker = new google.maps.Marker({
+			animation: google.maps.Animation.DROP,
+			position:{lat:place.lat, lng: place.lng},
+			map: map,
+			title: place.title
+		});
+
+		google.maps.event.addListener(marker, 'click', function(){
+			infowindow = new google.maps.InfoWindow({content: place.title});
+			infowindow.open(map, marker);
+		});
+	};
 
 	locations = locationInformation();
 	addMarkers();
