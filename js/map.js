35b9a2ad92cd1f25f6d606b8c643884701d5313b
Robert Garza <[robstudent1988@gmail.com]>
2015-09-14 08:04:32 -0700

    fixed infowindow, only 1 will show. Added infocliks to search list

--- js/map.js
+++ js/map.js
@@ -1,8 +1,9 @@
+var map;
+var allMarkers = [];
+
 function initMap(){
 	var infowindow = new google.maps.InfoWindow({});
-	var map;
 	var clovis = {lat: 36.8253, lng: -119.7031};
-	var allMarkers = [];
 
 	//Map initial location
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -19,8 +20,6 @@ function initMap(){
 	//NewCreate Markers
 	function createMarker(place){
 
-		var infowindow;
-
 		var marker = new google.maps.Marker({
 			animation: google.maps.Animation.DROP,
 			position:{lat:place.lat, lng: place.lng},
@@ -29,9 +28,50 @@ function initMap(){
 		});
 
 		google.maps.event.addListener(marker, 'click', function(){
-			infowindow = new google.maps.InfoWindow({content: place.title + "<br>" + place.location});
+			var contentString = place.title + "<br>" + place.location;
+			infowindow.setContent(contentString);
 			infowindow.open(map, this);
 		});
-	};
-};
 
+		allMarkers.push(marker);
+	};
+
+	//OATUH as found in https://github.com/bettiolo/oauth-signature-js
+	/*var httpMethod = 'GET',
+		yelp_url = 'http://api.yelp.com/v2/business/',
+		parameters = {
+			oauth_consumer_key: 'rAf_1-qI-AMixiABXuySng',
+			oauth_token: '43-6kJ5la1xKegLdE_nzoESLmEGXw4Xd',
+			oauth_nonce: 'kllo9940pd9333jh',
+			oauth_timestamp: Math.floor(Date.now()/1000),
+			oath_signature_method: 'HMAC-SHA1',
+			oath_version: '1.0',
+		},
+		consumerSecret = 't6WlnqnsT6o1yOfhCDlV2bENlbI',
+		tokenSecret = 'ZU8_bhU-B9u4c11dTYcUSdgOepc',
+		//Generates a RFC 3986 encoded, BASE64 encoded HMAC-SHA1 hash
+		encodedSignature = oauthSignature.generate(httpMethod, yelp_url, parameters, consumerSecret,
+			tokenSecret),
+		signature = oauthSignature.generate(httpMethod, yelp_url, parameters, consumerSecret, tokenSecret,
+			{encodedSignature: false});
+
+	var requestUrl;
+	var getYelp = function(place){
+		requestUrl = yelp_url + '?term=' + place.title + '&location=' + place.location;
+		console.log(requestUrl);
+		$.ajax({
+			method: "GET",
+			url : requestUrl,
+			dataType: 'jsonp',
+			success: function(){
+				console.log("success");
+			},
+			error: function(){
+				alert('Sorry, shits fucked');
+			}
+		})
+	};
+
+	getYelp(foodPlaces.restaurant[0]);*/
+
+};
--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,19 +1,23 @@
 var vm = {
-	foodLocations: ko.observableArray(foodPlaces.restaurant),
+	foodLocations: ko.observableArray(foodPlaces.restaurant.slice(0)),
 	query: ko.observable(''),
+	openInfo: function(entry){
+		var holdIndex = foodPlaces.restaurant.indexOf(entry);
+		google.maps.event.trigger(allMarkers[holdIndex], "click");
+	},
 	search: function(value){
 		//remove all the current locations, which removes them from the view
 		vm.foodLocations.removeAll();
 
-		if(value == '') return;
-
-		for(var x in foodLocations){
-			if (foodLocations[x].name.toLowerCase().indexOf(value.toLowerCase()) >=0) {
-				vm.foodLocations.push(foodLocations[x]);
+		for(var x in foodPlaces.restaurant){
+			if (foodPlaces.restaurant[x].title.toLowerCase().indexOf(value.toLowerCase()) >=0) {
+				vm.foodLocations.push(foodPlaces.restaurant[x]);
 			}
 		}
 	}
 };
-vm.query.subscribe(vm.search);
 
 ko.applyBindings(vm);
+
+vm.query.subscribe(vm.search);
+
