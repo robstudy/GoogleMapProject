e10953a28fe7f1e87efd3f348f67c485b7829dc3
Robert Garza <[robstudent1988@gmail.com]>
2015-09-15 07:16:49 -0700

    added img file with icons, created click functionality with icons

--- js/map.js
+++ js/map.js
@@ -20,18 +20,65 @@ function initMap(){
 	//NewCreate Markers
 	function createMarker(place){
 
+		//create a blue marker for google maps
+		var azureImage = {
+			url: 'img/azure.png',
+			scaledSize: new google.maps.Size(40,40)
+		};
+		//for pink marker
+		var pinkImage = {
+			url: 'img/pink.png',
+			scaledSize: new google.maps.Size(50, 50)
+		}
+
 		var marker = new google.maps.Marker({
 			animation: google.maps.Animation.DROP,
 			position:{lat:place.lat, lng: place.lng},
 			map: map,
-			title: place.title
+			title: place.title,
+			icon: azureImage
 		});
 
 		google.maps.event.addListener(marker, 'click', function(){
+			resetAllMarkers();
+			setClicked();
 			var contentString = place.title + "<br>" + place.location;
 			infowindow.setContent(contentString);
 			infowindow.open(map, this);
 		});
+
+		//reset markers function
+		function resetAllMarkers(){
+			for(var x in allMarkers){
+				if (allMarkers[x].clicked === true) {
+					allMarkers[x].clicked = false;
+					allMarkers[x].setIcon(azureImage);
+				};
+			}
+		}
+
+		//keep track of marker clicks
+		marker.clicked = false;
+
+		function setClicked(){
+			if (marker.clicked === false) {
+				marker.clicked = true;
+				marker.setIcon(pinkImage);
+			};
+		}
+
+		//mouse hover changes icon pink
+		marker.addListener('mouseover', function(){
+			if(marker.clicked === false){
+				marker.setIcon(pinkImage);
+			}
+		});
+		//sets icon back to blue when not hovered
+		marker.addListener('mouseout', function(){
+			if(marker.clicked === false){
+				marker.setIcon(azureImage);
+			}
+		})
 
 		allMarkers.push(marker);
 	};
@@ -67,11 +114,12 @@ function initMap(){
 				console.log("success");
 			},
 			error: function(){
-				alert('Sorry, shits fucked');
+				alert('Sorry, not gonna work');
 			}
 		})
 	};
 
 	getYelp(foodPlaces.restaurant[0]);*/
-
 };
+
+
